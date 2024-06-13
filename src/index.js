95932dedb64e2afb3ca1073ebbf4c1d845f3028d
Milan Khati <milankc4860@gmail.com>
2024-06-13 12:23:32 +0545

    netlify ci/cd

--- src/index.js
+++ src/index.js
@@ -1,26 +1,25 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const sendEmail = require("./services/emailService");
-const ejs = require("ejs");
 const { sendWelcomeEmail } = require("./controllers/emailController");
 const path = require("path");
 const app = express();
-const PORT = process.env.PORT || 3000;
-
-// Middleware to parse JSON bodies
+const serverless = require("serverless-http");
+const router = express.Router();
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "public")));
 
 // Welcome route
-app.get("/", (req, res) => {
+router.get("/welcome", (req, res) => {
   res.send("Welcome to the email sending service!");
 });
-
 // Route to send welcome email
-app.post("/send-email", sendWelcomeEmail);
+router.post("/", sendWelcomeEmail);
+
+app.use("/api", router);
 
 // Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+module.exports.handler = serverless(app);
+// app.listen(PORT, () => {
+//   console.log(`Server is running on http://localhost:${PORT}`);
+// });
